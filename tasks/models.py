469606b1c26e8c69b59cb8a94fa1eb361fefd171
vulkan3k <rodrigo.carrasco40@inacapmail.cl>
2022-12-28 01:48:30 -0300

    avanzado admin

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_completed= models.DateTimeField(null=True)
+    is_important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)#on delete para que si se elimina el usuario, se elimina la tarea tambien
+
+    def __str__(self):
+        return self.title + ' created by ' + self.user.username
